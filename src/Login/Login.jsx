5ede6f2e1fb2351e9d3e655e9081d2e5985d5045
FeresBenBrahim <93704692+FeresBenBrahim@users.noreply.github.com>
2023-11-07 12:05:20 +0100

    Update Login.jsx

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,38 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 import './login.css'
 import { setUser } from '../store/slices/userSlice';
-const fs = require('fs');
-const path = require('path');
-
 function Login() {
-
-    const writeFile = async () =>{
-    const fileUrl = 'https://www.africau.edu/images/default/sample.pdf'; // URL of the file to download
-const downloadDirectory = 'C:/newfolder/testttt'; 
-await fs.promises.mkdir(downloadDirectory, { recursive: true });
-const response = await axios.get(fileUrl, {
-  responseType: "stream",
-});
-const fileName = path.basename(fileUrl);
-const filePath = path.join(downloadDirectory, fileName);
-        console.log(filePath);
-const fileStream = fs.createWriteStream(filePath);
-response.data.pipe(fileStream);
-
-fileStream.on("finish", () => {
-  fileStream.close();
-  console.log(
-    `File '${fileName}' downloaded to the '${downloadDirectory}' folder on the desktop.`
-  );
-});
-
-}
-    console.log("hhiihihihi");
-    const handle = (e)=>{
-    e.preventDefault();
-        writeFile();
-}    
-    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loggedIn, setIsLoggedIn] = useState(false);
@@ -83,10 +52,7 @@ fileStream.on("finish", () => {
                 </label>
             </span>
         </div>
-        <button onClick={(e)=>handle(e)}>Click meeeee</button>
     </form>
-        <button onClick={(e)=>handle(e)}>Click meeeee</button>
-        <button onClick={(e)=>handle(e)}>Click meeeee</button>
 </div>
 }
 
